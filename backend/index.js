90cbb27341e7ba2632f0cc338f31a5b03041d1b9
PriyanshuSharma0326 <ps940016@gmail.com>
2024-02-18 18:59:45 +0530

    Update link in index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://socket-chat-frontend-blue.vercel.app/',
+        origin: 'https://socket-chat-frontend-blue.vercel.app',
         methods: [
             'GET',
             'POST'
